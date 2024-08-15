9e809e8377e3b8b3e492722168ef843dfa34a403
Vitor Reis <reis_vitor@outlook.com>
2024-08-15 19:42:55 -0300

    refactor: url model database key

--- models/url.go
+++ models/url.go
@@ -6,7 +6,7 @@ import (
 
 type Url struct {
 	ID        string    `bson:"_id" json:"id"`
-	FullUrl   string    `bson:"name" json:"full-url"`
+	FullUrl   string    `bson:"full-url" json:"full-url"`
 	ExpiresAt time.Time `bson:"expires-at" json:"expires-at"`
 	CreatedAt time.Time `bson:"created-at" json:"created-at"`
 }
