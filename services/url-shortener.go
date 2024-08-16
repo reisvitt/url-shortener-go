5fa67a3533858b134b613023d91031568ffb1b01
Vitor Reis <reis_vitor@outlook.com>
2024-08-15 23:33:57 -0300

    refactor: url shortener service

--- services/url-shortener.go
+++ services/url-shortener.go
@@ -1,8 +1,10 @@
 package services
 
 import (
+	"github.com/reisvitt/url-shortener-go/dto"
 	"github.com/reisvitt/url-shortener-go/models"
 	"github.com/reisvitt/url-shortener-go/repository"
+	"github.com/reisvitt/url-shortener-go/utils"
 )
 
 type UrlService struct {
@@ -13,16 +15,50 @@ func NewUrlService(repository repository.UrlRepository) *UrlService {
 	return &UrlService{repo: repository}
 }
 
-func CreateShortenUrl(url *models.Url) (*models.Url, error) {
-	// create a new shorten url
-	// validate the shorten url
+func (s *UrlService) CreateShortenUrl(urlDTO *dto.UrlDTO) (*models.Url, error) {
+
+	url := &models.Url{
+		FullUrl: urlDTO.Url,
+	}
+
+	var shortenUrl string
+
+	for {
+		// create a new shorten url
+		random, err := utils.GerenateRandomPath()
+		if err != nil {
+			return nil, err
+		}
+
+		shortenUrl = random
+
+		// validate the shorten url
+		has, _ := s.repo.GetByID(&shortenUrl)
+		if has == nil {
+			break
+		}
+	}
+
 	// store the shorten url in the database
+	url.ID = shortenUrl
+	dabatabaseUrl, err := s.repo.Create(url)
+
+	if err != nil {
+		return nil, err
+	}
+
 	// return the shortened url
-	return nil, nil
+	return dabatabaseUrl, nil
 }
 
-func GetShortenedUrl(path *string) (*models.Url, error) {
+func (s *UrlService) GetShortenedUrl(path *string) (*string, error) {
 	// get the full url from the database based on the shortened path
+	url, err := s.repo.GetByID(path)
+
+	if err != nil {
+		return nil, err
+	}
+
 	// return the full url
-	return nil, nil
+	return &url.FullUrl, nil
 }
